22c2a68b260ca92bed417cdd15d177afdb270917
Adam Lindberg <hello@alind.io>
2022-12-08 09:21:41 +0100

    Reduce duplication in title case test suite

--- test/textual_titlecase_tests.erl
+++ test/textual_titlecase_tests.erl
@@ -2,6 +2,8 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+%--- Macros --------------------------------------------------------------------
+
 -define(SHORT_CONJUCTIONS, [
     "and", "as", "but", "for", "if", "nor", "or", "so", "yet"
 ]).
@@ -9,6 +11,9 @@
 -define(SHORT_PREPOSITIONS, [
     "as", "at", "by", "for", "in", "of", "off", "on", "per", "to", "up", "via"
 ]).
+
+-define(equal(A, B), ?assertEqual(to_bin(A), to_bin(B))).
+-define(_equal(A, B), fun() -> ?equal(to_bin(A), to_bin(B)) end).
 
 %--- Tests ---------------------------------------------------------------------
 
@@ -30,53 +35,55 @@ titlecase_apa_test_() ->
     {inparallel,
         lists:flatten([
             [
-                ?_assertEqual(to_bin(U), to_bin(textual:titlecase(L))),
-                ?_assertEqual(to_bin(U), to_bin(textual:titlecase(to_bin(L))))
+                ?_equal(U, textual:titlecase(L)),
+                ?_equal(U, textual:titlecase(to_bin(L)))
             ]
-         || {U, L} <-
-                [
-                    {
-                        "FYI: Erlang and OTP Together Are Erlang/OTP, Not Erlang/Erlang",
-                        "FYI: erlang and OTP together are erlang/OTP, not erlang/erlang"
-                    },
-                    {
-                        "The Quick Brown Fox Jumps Over the Lazy Dog. Not the Bear.",
-                        "the quick brown fox jumps over The lazy dog. not the bear."
-                    },
-                    {
-                        "Turning Frowns (and Smiles) Upside Down: A Multilevel Examination of Surface Acting Positive and Negative Emotions on Well-Being",
-                        "Turning Frowns (And Smiles) Upside Down: a Multilevel Examination of Surface Acting Positive and Negative Emotions on Well-being"
-                    },
-                    {
-                        "Train Your Mind for Peak Performance: A Science-Based Approach for Achieving Your Goals",
-                        "train your mind For peak performance: a science-based approach For achieving your goals"
-                    }
-                ] ++
-                    [
-                        {
-                            lists:join($\s, [
-                                string:titlecase(W), W, string:titlecase(W)
-                            ]),
-                            lists:join($\s, [W, W, W])
-                        }
-                     || W <-
-                            ?SHORT_CONJUCTIONS ++ ?ARTICLES ++
-                                ?SHORT_PREPOSITIONS
-                    ]
+         || {U, L} <- [
+                {
+                    lists:join($\s, [
+                        string:titlecase(W), W, string:titlecase(W)
+                    ]),
+                    lists:join($\s, [W, W, W])
+                }
+             || W <- ?SHORT_CONJUCTIONS ++ ?ARTICLES ++ ?SHORT_PREPOSITIONS
+            ]
         ])}.
 
 assume_abbr_test() ->
-    ?assertEqual(
-        to_bin(
-            "Fyi: Erlang and Otp Together Are Erlang/Otp, Not Erlang/Erlang"
-        ),
-        to_bin(
-            textual:titlecase(
-                "FYI: erlang and OTP together are erlang/OTP, not erlang/erlang",
-                #{abbr => capitalize}
-            )
+    ?equal(
+        "Fyi: Erlang and Otp Together Are Erlang/Otp, Not Erlang/Erlang",
+        textual:titlecase(
+            "FYI: erlang and OTP together are erlang/OTP, not erlang/erlang",
+            #{abbr => capitalize}
         )
     ).
+
+sentence_test_() ->
+    {inparallel,
+        lists:flatten([
+            [
+                ?_equal(U, textual:titlecase(L)),
+                ?_equal(U, textual:titlecase(to_bin(L)))
+            ]
+         || {U, L} <- [
+                {
+                    "FYI: Erlang and OTP Together Are Erlang/OTP, Not Erlang/Erlang",
+                    "FYI: erlang and OTP together are erlang/OTP, not erlang/erlang"
+                },
+                {
+                    "The Quick Brown Fox Jumps Over the Lazy Dog. Not the Bear.",
+                    "the quick brown fox jumps over The lazy dog. not the bear."
+                },
+                {
+                    "Turning Frowns (and Smiles) Upside Down: A Multilevel Examination of Surface Acting Positive and Negative Emotions on Well-Being",
+                    "Turning Frowns (And Smiles) Upside Down: a Multilevel Examination of Surface Acting Positive and Negative Emotions on Well-being"
+                },
+                {
+                    "Train Your Mind for Peak Performance: A Science-Based Approach for Achieving Your Goals",
+                    "train your mind For peak performance: a science-based approach For achieving your goals"
+                }
+            ]
+        ])}.
 
 %--- Internal ------------------------------------------------------------------
 
